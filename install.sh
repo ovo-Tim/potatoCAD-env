3d8febcd0c94ff4d6baa8ab20c99412e23484215
ovo-tim <ovo-tim@qq.com>
2023-06-29 14:41:24 +0800

    fix bug

---
+++ install.sh
@@ -0,0 +1,11 @@
+mamba create -n potatoCAD python=3.8
+conda init
+if ["$(uname)"=="Darwin"];then
+    source /Users/runner/.bash_profile
+elif ["$(expr substr $(uname -s) 1 5)"=="Linux"];then   
+    source /home/runner/.bashrc
+fi
+conda activate potatoCAD
+python -m pip install --upgrade pip
+pip install -r ./requirements-pip.txt
+mamba install --yes --file ./requirements-conda.txt 
